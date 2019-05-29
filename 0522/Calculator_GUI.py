5adfe0e3b7292da316657b34cbc8027eebe26c9b
YakumoRan <yys14239103@gmail.com>
2019-05-29 14:44:12 +0900

    0529

--- 0522/Calculator_GUI.py
+++ 0522/Calculator_GUI.py
@@ -1,11 +1,44 @@
 from tkinter import *
 
+
+
 def add() :
-    pass
+    result["text"] = str(int(Op[0].get()) + int(Op[1].get()))
+
+def sub() :
+    result["text"] = str(int(Op[0].get()) - int(Op[1].get()))
+
+def mul() :
+    result["text"] = str(int(Op[0].get()) * int(Op[1].get()))
+
+def div() :
+    result["text"] = str(int(Op[0].get()) / int(Op[1].get()))
 
 window = Tk()
 
 window.title("계산기")
-window.geometry("300 x 200")
 window.resizable(width = FALSE, height = TRUE)
 
+opFrame = Frame(window)
+buttonsFrame = Frame(window)
+resultFrame = Frame(window)
+
+opFrame.pack()
+
+Op = list()
+for i in range(2) :
+    Op.append(Entry(opFrame))
+    Op[i].pack(side = LEFT)
+
+result = Label(resultFrame, width = 5, height = 1, anchor = NE)
+
+buttonsFrame.pack()
+Buttons = dict()
+for v in ["add", "sub", "mul", "div"] :
+    Buttons[v] = Button(buttonsFrame, text = v, width = 3, height = 3, command = globals()[v])
+    Buttons[v].pack(side = LEFT)
+
+resultFrame.pack()
+result.pack()
+
+window.mainloop()
